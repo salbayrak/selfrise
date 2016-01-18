3a40db039cfc28979a44ac4cb60efbff08447552
salbayrak <salbayrak@ebay.com>
2016-01-18 17:06:40 +0200

    home page and some images are added

---
+++ app/controllers/HomeController.scala
@@ -0,0 +1,17 @@
+package controllers
+
+import javax.inject.Inject
+
+import play.api.mvc._
+import play.api.i18n._
+
+/**
+  * Created by salbayrak on 18/01/16.
+  */
+class HomeController @Inject() (val messagesApi: MessagesApi) extends Controller with I18nSupport{
+
+  def index = Action {
+    Ok(views.html.home())
+  }
+
+}
